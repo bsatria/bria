12224f9737eb6b470d3c7b08a556f170d0c4448d
bsatria <bsatriak@koinworks.com>
2023-01-14 16:50:22 +0700

    add some of route

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   resolve: {
     extensions: ['.mjs', '.js', '.jsx', '.json', '.tsx'],
   },
