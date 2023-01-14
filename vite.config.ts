7974ac8169316fed1c6c4cc55539a64be286fa42
bsatria <bsatriak@koinworks.com>
2023-01-14 16:46:31 +0700

    remove split

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   resolve: {
     extensions: ['.mjs', '.js', '.jsx', '.json', '.tsx'],
   },
